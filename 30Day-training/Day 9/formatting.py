b0bafbd45684a7fccac3791a88a399799747fab3
Tejas <tejasravi51@gmail.com>
2020-04-12 02:16:18 +0530

    *

--- 30Day-training/Day 9/formatting.py
+++ 30Day-training/Day 9/formatting.py
@@ -3,6 +3,6 @@ Thank you for joining {website}. We are very
 happy to have you with us.
 """ # .format(name="Justin", website='cfe.sh') without using the function
 
-def format_msg(my_name="Justin", my_website="cfe.sh"):
+def format_msg(my_name="Dev tez", my_website="t&tinc.org"):
     my_msg = msg_template.format(name=my_name, website=my_website)
     return my_msg 
