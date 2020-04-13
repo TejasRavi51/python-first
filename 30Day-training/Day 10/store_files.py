24a12d4597dd63fc50f5067b425e286c7dea7fcc
Tejas <tejasravi51@gmail.com>
2020-04-13 13:05:36 +0530

    day10

--- 30Day-training/Day 10/store_files.py
+++ 30Day-training/Day 10/store_files.py
@@ -1,7 +1,7 @@
 import os
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
-files_dir = os.path.join(base_dir, "images")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+files_dir = os.path.join(BASE_DIR, "images")
 
 #if not os.path.exists(files_dir):
 #    print("This is not a valid path")
