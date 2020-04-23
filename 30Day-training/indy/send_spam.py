cfb4e4146032fee836b2114b6d926afd0d4478b7
Tejas <tejasravi51@gmail.com>
2020-04-24 01:44:41 +0530

    indy transfer and rename files or delete

---
+++ 30Day-training/indy/send_spam.py
@@ -0,0 +1,8 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+#from templates import Template
+# environment variables
+username = 'hungrydev51@gmail.com'
+password = '!Goofball420'
